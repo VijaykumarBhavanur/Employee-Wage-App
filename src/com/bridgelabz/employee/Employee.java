d498c1047175ea2062b4a55b13b30b31787d80d5
VijaykumarBhavanur <vijaykumarbhavanur@gmail.com>
2021-08-26 08:50:37 +0530

    Added: First line of code

--- src/com/bridgelabz/employee/Employee.java
+++ src/com/bridgelabz/employee/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation App..");
+		System.out.println("This is my first line of code..");
 
 	}
 
